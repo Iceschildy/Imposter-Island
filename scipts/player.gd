8d12283cd9db694503a8fabdabd78ceb187ca707
Louis Stöhr <iceschildy@gmail.com>
2024-06-23 19:51:51 +0200

    Added PlayerMov

---
+++ scipts/player.gd
@@ -0,0 +1,62 @@
+extends CharacterBody3D
+
+var speed 
+const WALK_SPEED = 5.0
+const sprint_speed = 8.0
+const JUMP_VELOCITY = 4.5
+const sensitivity = 0.003
+
+const Base_Fov = 75.0
+const Fov_Change = 1.5
+
+
+var gravity = 9.8
+
+@onready var head = $Head
+@onready var cam = $Head/Camera3D
+
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		head.rotate_y(-event.relative.x * sensitivity)
+		cam.rotate_x(-event.relative.y * sensitivity)
+		cam.rotation.x = clamp(cam.rotation.x, deg_to_rad(-40), deg_to_rad(60))
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	if Input.is_action_pressed("Sprint"):
+		speed = sprint_speed
+	else:
+		speed = WALK_SPEED
+
+
+	var input_dir = Input.get_vector("Left", "Right", "Up", "Down")
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if is_on_floor():
+		if direction:
+			velocity.x = direction.x * speed
+			velocity.z = direction.z * speed
+		else:
+			velocity.x = 0.0
+			velocity.z = 0.0
+	else :
+		velocity.x = lerp(velocity.x, direction.x * speed, delta * 7.0)
+		velocity.z = lerp(velocity.z, direction.z * speed, delta * 7.0)
+		
+	
+	var velocity_clamped = clamp(velocity.length(), 0.5, sprint_speed * 2)
+	var traget_fov = Base_Fov + Fov_Change * velocity_clamped
+	cam.fov = lerp(cam.fov, traget_fov, delta * 8.0)
+
+	move_and_slide()
