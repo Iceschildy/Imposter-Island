262038e3d06f68488e7c141c67db7c174bed2634
NeikelDev <seemann.nikita@gmail.com>
2024-07-07 16:17:36 +0200

    Added Wood model and applied gravity to it

---
+++ scipts/wood.gd
@@ -0,0 +1,14 @@
+extends Area3D
+
+
+var gravity_items := 9.8
+
+@onready var ray_cast_3d = $RayCast3D
+
+
+func _physics_process(delta):
+	if ray_cast_3d.is_colliding():
+		pass
+	else:
+		position.y -= gravity_items * delta
+
